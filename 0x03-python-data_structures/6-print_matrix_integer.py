14957a01f0838f4f84124b61e3b3a784bb418686
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-08-14 19:33:14 +0300

    6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        count = 0
+        for j in i:
+            if count < 2:
+                print(j, end=" ")
+            else:
+                print(j, end="")
+            count = count + 1
+        print('')
