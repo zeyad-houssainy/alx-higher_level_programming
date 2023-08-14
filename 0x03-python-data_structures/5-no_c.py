89e039514e6991b58cf11c77f904f8583c8775ac
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-08-14 19:02:11 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = list(my_string)
-    # print(my_string)
+    new_string = []
     for i in my_string:
-        print(i)
-        if i == 'c' or i == 'C':
-            print("XXXX")
-            my_string.remove(i)
-    my_string = "".join(my_string)
-    return my_string
-
-
-    
-    
-    
-
-
-
-print(no_c("ABcCD"))
-# print(no_c("Chicago"))
-# print(no_c("C is fun!"))
+        if i != 'c' and i != 'C':
+            new_string.append(i)
+    new_string = "".join(new_string)
+    return (new_string)
