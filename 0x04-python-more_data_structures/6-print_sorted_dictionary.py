7b791cb724827ad408d37f254df9d111870b2081
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-08-20 16:44:19 +0300

    0

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    
 
+
+a_dictionary = {'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3]}
+print_sorted_dictionary(a_dictionary)
---
+++ 0x04-python-more_data_structures/test.py
@@ -0,0 +1,15 @@
+a = {"Z": 1, "b": 2, "c": 3, "d": "zeyad", "x": "Ahemd"}
+# print a
+# print a.keys()
+# print(sorted([i for i in a.values()]))
+# print a.__sizeof__
+# for i in a.values():
+#     print (i)
+
+
+b = [1, 3, 2, 4, 6]
+c = ["b", "d", "c", "a"]
+e = ["b", "d", "c", "a", 1, 3, 2, 4, 6]
+print(sorted(b))
+print(sorted(c))
+print(sorted(e))
