d3ef839fb255ed571d6e6e8ad77ab04ebe45c3dc
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-08-21 22:45:06 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    max_value = 0
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    else:
+        for k, v in a_dictionary.items():
+            if v > max_value:
+                max_value = v
+        return (max_value)
