53a3848574c548e675622c9605d57dcc3ba8b32d
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-08-22 23:09:02 +0300

    0

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string == None:
+        return (0)
+    roman_dict = {  'I': 1,
+                    'V': 5,
+                    'X': 10,
+                    'L': 50,
+                    'C': 100,
+                    'D': 500,
+                    'M': 1000
+                }
+    for i in roman_string:
+
+# This code is not done yet
