861637e2211cbfebb5546421697f07b097e2caa4
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-08-30 19:21:40 +0300

    3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""define an empty class square"""
+
+
+class Square:
+    """square class is generated"""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
