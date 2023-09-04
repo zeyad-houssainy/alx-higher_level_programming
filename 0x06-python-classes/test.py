48abf44a27abcd9e9e1a6e64608d7513101647bc
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-09-05 02:35:11 +0300

    0

--- 0x06-python-classes/test.py
+++ 0x06-python-classes/test.py
@@ -1,44 +1,49 @@
-class Square:
-    """Represent a square."""
+class Human:
+    def __init__(self, name="Not assigned", age=0, gender="None"):
+        self.name = name
+        self.age = age
+        if gender.lower() == "female" or gender.lower() == "male":
+            self.gender = gender
+        else:
+            raise ValueError(f"Gender for '{self.name}' must be either 'Male' or 'Female'")
+    def greet(self):
+        print(f"Hello i'm {self.name} and i'm {self.age} old\nI'M a {self.gender}")
 
-    def __init__(self, size=0):
-        self.size = size
+    def __str__(self):
+        """to use print function on it"""
+        return f"i'm a new human. my code name is {self.name}"
 
-    @property
-    def size(self):
-        return (self.__size)
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+p1 = Human("Zeyad", 25, "Male")
+# print(p1.name)
+# p1.greet()
+# p1.name = "Mohamed"
+# print(p1.name)
+# p1.greet()
+p2 = Human("habiba", 15, "Female")
+# print(p1)
+# print(p2)
 
-    def area(self):
-        return (self.__size * self.__size)
+class Dog:
+    #basic info about dogs
+    sound = "Roff Roff"
+    leg_count = 4
 
-################################################################
-################################################################
-################################################################
-################################################################
+    def __init__(self, name, type, color):
+        self.__name = name
+        self.color = color
+        self.type = type
 
-class Square:
-    """square class is generated"""
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self, new_name):
+        self.__name = new_name
 
-    def __init__(self, size=0):
-        self.size = size
+    
 
-    def size(self):
-        return (self.__size)
+# print(Dog.sound)
+dog1 = Dog("Bob","German shephard", "White")
 
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
 
-    def area(self):
-        return (self.__size * self.__size)
+
