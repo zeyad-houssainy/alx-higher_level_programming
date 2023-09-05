5256db243284facc449420a2830ae9de7eaed857
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-09-05 21:08:21 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+"""define an empty class square"""
+
+
+class Square:
+    """square class is generated"""
+
+    def __init__(self, size=0):
+        """initiate attributes"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """setter for private __size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
