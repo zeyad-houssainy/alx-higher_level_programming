2a90351b4172d2381f933cff169bcbf15d122e71
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-09-05 17:26:06 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,12 +9,10 @@ class Square:
         """initiate attributes"""
         self.size = size
 
-
     @property
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-
 
     @size.setter
     def size(self, value):
