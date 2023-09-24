8c22ae9078175fcc1c038f63822e0927abd0c465
Zeyad Al-Houssainy <zeyad-houssainy@hotmail.com>
2023-09-24 15:59:22 +0300

    0

--- 0x09-python-everything_is_object/test.py
+++ 0x09-python-everything_is_object/test.py
@@ -1,3 +1,19 @@
-a = 89
+#l1 = [1, 2, 3]
+#l2 = l1
+#print(l1 is l2)
+
+a = "Zeyad"
+print(id(a))
+a = "Mohamed"
+print(id(a))
+a = [1,2,3]
+print(id(a))
+a[0] = 8
+print(id(a))
 b = a
-print(a is b)
+print(id(a))
+print(id(b))
+b = "Z"
+print(id(a))
+print(id(b))
+
