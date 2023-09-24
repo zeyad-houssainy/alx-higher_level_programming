4971545b637b97d19d85c8653901ba5725d29494
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-09-24 22:13:33 +0300

    0

--- 0x09-python-everything_is_object/test.py
+++ 0x09-python-everything_is_object/test.py
@@ -1,19 +1,7 @@
-#l1 = [1, 2, 3]
-#l2 = l1
-#print(l1 is l2)
+def increment(n):
+    n.append(4)
 
-a = "Zeyad"
-print(id(a))
-a = "Mohamed"
-print(id(a))
-a = [1,2,3]
-print(id(a))
-a[0] = 8
-print(id(a))
-b = a
-print(id(a))
-print(id(b))
-b = "Z"
-print(id(a))
-print(id(b))
 
+l = [1, 2, 3]
+increment(l)
+print(l)
