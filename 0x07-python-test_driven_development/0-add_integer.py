a70aa93cef616c454b0fe50834858544246b2c56
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-10-04 00:39:45 +0300

    0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,7 +3,33 @@
 
 
 def add_integer(a, b=98):
-    """return adding the integer a and b"""
+    """return adding the integer a and b
+    
+    >>> add_integer(1,1)
+    2
+    >>> print(add_integer(1, 2))
+    3
+    >>> print(add_integer(100, -2))
+    98
+    >>> print(add_integer(2))
+    100
+    >>> print(add_integer(100.3, -2))
+    98
+    >>> print(add_integer(4, "School"))
+    Traceback (most recent call last):
+    TypeError: b must be an integer
+    >>> print(add_integer(None))
+    Traceback (most recent call last):
+    TypeError: a must be an integer
+    >>> add_integer(float('nan'))
+    Traceback (most recent call last):
+    >>> add_integer(2, float('nan'))
+    Traceback (most recent call last):
+    ValueError: cannot convert float NaN to integer
+    >>> add_integer(float('inf'))
+    Traceback (most recent call last):
+    OverflowError: cannot convert float infinity to integer
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+def matrix_divided(matrix, div):
+    new_matrix = []
+    #check matrix is list
+    if not (type(matrix) is list) or not (all(type(i) is list for i in matrix)):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    #check if list lenght are equal:
+    for i in matrix:
+        if len(i) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+    #check if div is integer or float
+    if not (type(div) is int or type(div) is float):
+        raise TypeError("div must be a number")
+    elif div == 0:
+        raise ZeroDivisionError("division by zero")
+    
+    for i in range(len(matrix)):
+        new_matrix.append([])
+        for k in range(len(matrix[i])):
+            # x = 
+            new_matrix[i][k] = (new_matrix[i]).append(round((matrix[i][k] / 3)), div)
+            # new_matrix[i][k] = round(x, 2)
+    print(f"new matrix is: {new_matrix}")
+    print(f"Original matrix is: {matrix}")
+    print(id(matrix))
+    print(id(new_matrix))
+    print(matrix is new_matrix)
+    return (new_matrix)
+    
+    
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+# print(matrix_divided(matrix, 3))
+# print(matrix)
+matrix_divided(matrix, 3)
