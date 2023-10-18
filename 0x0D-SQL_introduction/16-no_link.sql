b88b0e3896b266679a8eefffd07cb6d2df10b598
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-10-18 20:14:48 +0300

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,4 @@
 -- Lists rows with name value
-SELECT score, name FROM second_table WHERE name != NULL ORDER BY score DESC;
+SELECT score, name FROM second_table 
+WHERE name != "" OR name IS NOT NULL
+ORDER BY score DESC;
