9ef97cbe9b9ff3087a33877d5ee1d83ab566517d
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-10-18 17:42:29 +0300

    0

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- List by best score
 SELECT score, name FROM second_table
-ORDER BY score;
+ORDER BY score DESC;
--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- List the score greather than or equal to 10
 SELECT name, score FROM second_table
 WHERE score >= 10
-ORDER BY score;
+ORDER BY score DESC;
