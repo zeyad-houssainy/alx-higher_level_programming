5d7ada5f3e0b186fe9d69c06511ada232dab60f4
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-10-19 03:12:17 +0300

    Create 6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- creates the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    id INT PRIMARY UNIQUE AUTO_INCREMENT NOT NULL,
+    name  VARCHAR(256) NOT NULL);
