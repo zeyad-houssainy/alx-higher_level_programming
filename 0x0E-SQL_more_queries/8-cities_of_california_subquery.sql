49a21c9c0f69332d63a36a55e3e774b787b16849
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-10-19 22:38:23 +0300

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,5 @@
 -- Create a subquery
 -- Execute: cat 8-cities_of_california_subquery.sql | mysql -hlocalhost -uroot -p hbtn_0d_usa
-
+SELECT hbtn_0d_usa.* 
+WHERE name = California
+ORDER BY cities.id ASC;
