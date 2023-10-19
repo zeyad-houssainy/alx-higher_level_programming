38dd3d58552fe08eb8898e0de9b967319243f74c
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-10-20 02:10:49 +0300

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,7 @@
--- Create a subquery
--- Execute: cat 8-cities_of_california_subquery.sql | mysql -hlocalhost -uroot -p hbtn_0d_usa
-SELECT name FROM hbtn_0d_usa.*
-WHERE name = California
-ORDER BY cities.id ASC;
+--Write a script that lists all the cities of California
+SELECT id, name FROM cities
+WHERE state_id = (
+      SELECT id FROM states
+      WHERE name = "California"
+      ORDER BY cities.id
+);
