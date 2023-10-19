859ee5a00654d1e1c26cd177f9b2baf3c8ed6326
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-10-20 02:51:52 +0300

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Use INNER JOIN
 -- Execute: cat 9-cities_by_state_join.sql | mysql -hlocalhost -uroot -p hbtn_0d_usa
 
-SELECT cities.id, cities.name, states.name FROM cities;
+SELECT cities.id, cities.name, states.name FROM cities
 JOIN states ON cities.state_id=states.id
 ORDER BY cities.id ASC;
