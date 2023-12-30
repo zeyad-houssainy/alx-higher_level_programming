d00a84ffb0ae707066f04100606dc5253aa2a4ce
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-12-31 00:53:36 +0200

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-if (process.argv[2] && !NaN(process.argv[2])) {
+
+if (process.argv[2] && !isNaN(process.argv[2])) {
   for (let i = 0; i < process.argv[2]; i++) { console.log(process.argv[2]); }
 } else {
   console.log('Missing size');
