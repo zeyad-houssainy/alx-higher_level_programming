4037bc1975e52134a6f0f18a8edcf9cd44e0f597
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-12-31 01:10:53 +0200

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 if (process.argv[2] && !isNaN(process.argv[2])) {
   for (let i = 0; i < process.argv[2]; i++) {
     for (let j = 0; j < process.argv[2]; j++) {
-      console.log(process.argv[2]);
+      console.log('X');
     }
     console.log('\n')
   }
