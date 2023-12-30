5509102541a87b2f962c6c3703f360da19fae717
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-12-30 09:33:19 +0200

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+if (process.argv[2] && !isNaN(process.argv[2])) {
+  for (const i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
