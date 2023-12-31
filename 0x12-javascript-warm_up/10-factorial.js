5265823980c6d6fc4917c0708858dd0177b3b3b9
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2023-12-31 23:47:56 +0200

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+function factorial (number) {
+  if (Number(number === 0) || Number(number === 1) || isNaN(number)) {
+    return 1;
+  } else {
+    return (Number(number * factorial(number - 1)));
+  }
+}
+
+console.log(factorial(process.argv[2]));
