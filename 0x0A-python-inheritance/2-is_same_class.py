13ef3b1a58e04582b66d4449e39782891b471060
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-02 23:09:35 +0200

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,9 +8,10 @@ def is_same_class(obj, a_class):
     Args:
         obj: the object to be checked
         a_class (type): the original parent class that we want to compare with
-    returns:
+    returns: 
         return True if the Obect is instance of a_class
     """
-    if obj.__name__ == a_class:
+    if type(obj) == a_class:
         return True
     return False
+
