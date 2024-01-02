6869d9e6d6c74542e9e65b2bc3310c044d5e7c14
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-02 22:54:15 +0200

    2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,4 +2,15 @@
 """Defines a class-checking function."""
 
 
+def is_same_class(obj, a_class):
+    """check if an object is an instance of the main given class
 
+    Args:
+        obj: the object to be checked
+        a_class (type): the original parent class that we want to compare with
+    returns:
+        return True if the Obect is instance of a_class
+    """
+    if obj.__name__ == a_class:
+        return True
+    return False
