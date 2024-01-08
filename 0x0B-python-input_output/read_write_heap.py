8f00efac19062c62be090f1801eecd1b7f17e8a4
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-08 13:22:39 +0200

    1

--- 0x0B-python-input_output/read_write_heap.py
+++ 0x0B-python-input_output/read_write_heap.py
@@ -1,78 +1,46 @@
 #!/usr/bin/python3
-"""Find and replace a string in the heap of a running process.
 
-Usage: `read_write_heap.py pid search_string replace_string`
-Where:
-    - pid is the PID of the running process
-    - strings are represented in ASCII.
-"""
-import sys
+myfile = open("test.txt", "w+")
+myfile.write(
+    "Hello there i'm testing how to open and write a file from scratch")
+# myfile = open
+myfile = open("test.txt", "r+")
+
+read_my_file = myfile.read()
+print(read_my_file)
+
+myfile.seek(0)
+myfile.write("Starting the file\n")
+
+myfile.seek(0, 2)
+myfile.write("\nwriting by the end of the file")
+
+# myfile.close()
+# myfile.open()
+
+# myfile = open("test.txt", "w+")
+# print(read_my_file)
+
+myfile = open("test.txt", "a+")
+# myfile.read()
+myfile.write("\nhihihih")
+read_lines = myfile.readlines()
 
 
-if len(sys.argv) != 4:
-    print("Usage: read_write_heap.py pid search_string replace_string")
-    sys.exit(1)
+for line in read_lines:
+    myfile.seek(0, 2)
+    myfile.write("zZz\n")
+    myfile.write(line)
+    myfile.write("\n")
 
-pid = sys.argv[1]
-search_string = sys.argv[2]
-replace_string = sys.argv[3] + "\0"
 
-print("Hacking a VM, eh? So you like to live dangerously... ")
+print(myfile.closed)
 
-print("  <> Searching for PID heap {}... ".format(pid), end="")
-address = ""
-maps_path = "/proc/" + pid + "/maps"
-try:
-    with open(maps_path, "r") as maps_file:
-        for line in maps_file:
-            if "[heap]" in line:
-                print("the heap has been located.")
+myfile.close()
 
-                if "r" not in line or "w" not in line:
-                    print("  [ERROR] PID heap lacks read/write permissions")
-                    sys.exit(0)
+print(myfile.closed)
 
-                line = line.split()
-                print("    >< pathname: {}".format(maps_path))
-                print("    >< address: {}".format(line[0]))
-                print("    >< permissions: {}".format(line[1]))
-                print("    >< offset: {}".format(line[2]))
-                print("    >< device: {}".format(line[3]))
-                print("    >< inode: {}".format(line[4]))
-                address = line[0].split('-')
 
-except IOError as e:
-    print("  [ERROR] cannot open file {}".format(maps_path))
-    print(e)
-    sys.exit(1)
-
-start = int(address[0], 16)
-end = int(address[1], 16)
-
-print("  <> Searching for string {}... ".format(search_string), end="")
-mem_path = "/proc/" + pid + "/mem"
-try:
-    with open(mem_path, "rb+") as mem_file:
-        mem_file.seek(start)
-        heap = mem_file.read(end - start)
-
-        try:
-            index = heap.index(search_string.encode("ASCII"))
-            print("the string has been located.")
-            print("    >< index: {}".format(index))
-        except Exception as e:
-            print("  [ERROR] cannot locate string {}".format(search_string))
-            print(e)
-            sys.exit(0)
-
-        print("  <> Replacing string {} ".format(search_string), end="")
-        print("with {}... ".format(replace_string[:-1]), end="")
-        mem_file.seek(start + index)
-        mem_file.write(replace_string.encode("ASCII"))
-        print("the string has been replaced.")
-
-except IOError:
-    print("  [ERROR] cannot write to file {}".format(mem_path))
-    sys.exit(1)
-
-print("Happy hacking, hacker!")
+with open("test.txt", "a+") as file:
+    x = file.read()
+    print(x)
