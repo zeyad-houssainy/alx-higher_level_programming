7b054f91d5a83a712a272160cb69baba2be2d358
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-09 22:29:05 +0200

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 """Defines a file-writing function."""
 
+
+def write_file(filename="", text=""):
+    """function to write a file"""
+
+    with open(filename, "w", encoding="utf-8") as f:
+        print(f.write(text))
