6fd2ba2660b10ee1146c911f2502b1cd27e5361c
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-09 21:57:22 +0200

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    with open("filename", "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
