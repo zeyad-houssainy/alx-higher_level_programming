ae987ede4fd9e4d7d373d82e125a7118a52bb4c2
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-10 00:09:48 +0200

    2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
-"""Defines a file-appending function."""
+"""Defines a file-writing function."""
 
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number
+    of characters written.
+
+    Args:
+        filename (str, optional): name of the file. Defaults to "".
+        text (str, optional): string of text to write to file. Defaults to "".
+
+    Returns:
+        int: number of characters written to file.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return (f.write(text))
