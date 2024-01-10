6a8093911389c7b51efc7d9888cccb0ef2453e33
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-10 14:31:44 +0200

    4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 """Defines a JSON-to-object function."""
+import json
 
+
+def from_json_string(my_str):
+    """Function that returns the JSON data into Data-structure type
+    Args:
+        my_str: given data
+
+    Returns:
+        Data structure type
+    """
+
+    return json.loads(my_str)
