955666e608c294b06596ea3ff5f90eee32a93794
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-10 15:11:07 +0200

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,7 +2,17 @@
 """Defines a JSON file-writing function."""
 import json
 
+
 def save_to_json_file(my_obj, filename):
+    """Function that returns the JSON data into Data-structure type
+    Args:
+        my_str: given data
+        filename: file that will write data on,
+        will create new file if not exists
+
+    Returns:
+        file contain JSON data
+    """
     with open(filename, "w", encoding="utf-8") as f:
         json_data = json.dumps(my_obj)
         f.write(json_data)
