0cbecffae29db40f77e7f68e864a962686635560
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-10 15:31:39 +0200

    Update 6-load_from_json_file.py

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-"""Defines a JSON file-reading function."""
+"""Turn Json data to the code"""
+import json
+
+
+def load_from_json_file(filename):
+    """Function that returns the JSON data into Data-structure type
+    Args:
+        filename: file with json data
+
+    Returns:
+        JSON data
+    """
+    with open(filename, "r", encoding="utf-8"):
+        json_data = json.load(filename)
+        return (json_data)
