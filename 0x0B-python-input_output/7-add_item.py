124a95183cb140f7f8bcb0b5c1ace9575a2049ff
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-11 18:10:01 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """adds all arguments to a Python list, and then save them to a file"""
 from sys import argv
-from json_module import load_from_json_file, save_to_json_file
 
+
+load_file = __import__("6-load_from_json_file").load_from_json_file
+save_file = __import__("5-save_to_json_file.py").save_to_json_file
 
 try:
-    json_list = load_from_json_file("add_item.json")
+    json_list = load_file("add_item.json")
 except (ValueError, FileNotFoundError):
     json_list = []
 
 for item in argv[1:]:
     json_list.append(item)
 
-save_to_json_file(json_list, "add_item.json")
+save_file(json_list, "add_item.json")
