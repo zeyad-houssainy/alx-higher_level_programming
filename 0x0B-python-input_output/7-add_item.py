67c782ab110a394c7d42f924c1d047ccd6a8f736
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-11 18:26:08 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ load_file = __import__("6-load_from_json_file").load_from_json_file
 save_file = __import__("5-save_to_json_file.py").save_to_json_file
 
 try:
-    json_list = load_file("add_item.json")
+    json_list = load_file()
 except (ValueError, FileNotFoundError):
     json_list = []
 
