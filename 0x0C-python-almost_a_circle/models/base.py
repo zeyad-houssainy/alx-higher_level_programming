27bb3e4c11e100310849df25516d61046d9db5f8
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-14 10:59:20 +0200

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects = __nb_objects + 1
