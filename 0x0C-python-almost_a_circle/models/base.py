090a374e9e8f4a08b3c0e901da52ecd27864624a
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-15 18:41:49 +0200

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+"""Module for the base class"""
 
 class Base:
-    '''A representation of the base of our OOP hierarchy.'''
+    """A representation of the base of our OOP hierarchy"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        '''Constructor.'''
+        """Constructor for the base model object
+        Attributes:
+            id (int): case for the ID for the constructor
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+
+
