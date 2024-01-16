8ed0e22273ca8febc83bd56820e7dc8af532bad8
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-01-16 23:43:33 +0200

    2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,64 +1,55 @@
 #!/usr/bin/python3
-'''Module for Rectangle class.'''
-from models.base import Base
+"""Module for the Rectangle class"""
+from base import Base
 
 
 class Rectangle(Base):
-    '''A Rectangle class.'''
+    """Rectangle Class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        '''Constructor.'''
+        """Constructor for rectangle"""
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
 
     @property
     def width(self):
-        '''Width of this rectangle.'''
+        """Getter for the width parameter"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        self.validate_integer("width", value, False)
+        """Setter for width"""
         self.__width = value
 
     @property
     def height(self):
-        '''Height of this rectangle.'''
+        """Getter for the width parameter"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        self.validate_integer("height", value, False)
+        """Setter for height"""
         self.__height = value
 
     @property
     def x(self):
-        '''x of this rectangle.'''
+        """Getter for the X parameter"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        self.validate_integer("x", value)
+        """Setter for X"""
         self.__x = value
 
     @property
     def y(self):
-        '''y of this rectangle.'''
+        """Getter for the Y parameter"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        self.validate_integer("y", value)
+        """Setter for Y"""
         self.__y = value
-
-    def validate_integer(self, name, value, eq=True):
-        '''Method for validating the value.'''
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if eq and value < 0:
-            raise ValueError("{} must be >= 0".format(name))
-        elif not eq and value <= 0:
-            raise ValueError("{} must be > 0".format(name))
--- 0x0C-python-almost_a_circle/test.py
+++ 0x0C-python-almost_a_circle/test.py
@@ -1,3 +1,4 @@
+from models.base import Base
 def test_function(var1, *argv):
     print(f"Variable Number 1: {var1}")
     count = 2
@@ -31,4 +32,66 @@ def greet_me(**kwargs):
 greet_me(name = "Raneem", age = 26)
 
 
+#!/usr/bin/python3
+'''Module for Rectangle class.'''
 
+
+class Rectangle(Base):
+    '''A Rectangle class.'''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        '''Constructor.'''
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        '''Width of this rectangle.'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.validate_integer("width", value, False)
+        self.__width = value
+
+    @property
+    def height(self):
+        '''Height of this rectangle.'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.validate_integer("height", value, False)
+        self.__height = value
+
+    @property
+    def x(self):
+        '''x of this rectangle.'''
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.validate_integer("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        '''y of this rectangle.'''
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.validate_integer("y", value)
+        self.__y = value
+
+    def validate_integer(self, name, value, eq=True):
+        '''Method for validating the value.'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
