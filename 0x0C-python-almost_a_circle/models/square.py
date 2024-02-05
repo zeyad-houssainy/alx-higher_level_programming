8d46d1a0d3f9a05d08cc4500496d2ead0779344e
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-02-05 19:56:32 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,57 +1,29 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
-
-
-from inspect import classify_class_attrs
+"""Module for Square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Class that defines properties of Square.
-
-     Attributes:
-        width (int): width of rectangle.
-        height (int): height of rectangle.
-        x (int): x.
-        y (int): y.
-        id (int): identity of square.
-    """
+    """Class define properties of a square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Creates new instances of Square
-
-        Args:
-            size (int): width and height of square.
-            x (int, optional): x. Defaults to 0.
-            y (int, optional): y. Defaults to 0.
-            id (int, optional): Identity number of square. Defaults to None.
-        """
+        """Create instances of a square"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Prints square"""
-        return ("[Square] ({}) {:d}/{:d} - {:d}".
-                format(self.id, self.x, self.y, self.size))
+        '''Returns string info about this square.'''
+        return '[{}] ({}) {}/{} - {}'.\
+            format(self.__class__.__name__, self.id,
+                   self.x, self.y, self.width)
 
     @property
     def size(self):
-        """Property retriever for size.
-
-        Returns:
-            int: size of one side of square.
-        """
+        """getter for size parameter"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Property setter for width of square.
-        Args:
-            value (int): width of square.
-        Raises:
-            TypeError: if width is not an integer.
-            ValueError: if width is less than or equal to zero.
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """setter for size"""
+        self.validate_integer("size", value)
+        self.width = value
+        self.height = value
