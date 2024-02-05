1bb4701a116c3745ef1ffdab655e5086b2f6812e
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-02-05 20:54:17 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,7 +19,7 @@ class Square(Rectangle):
     @property
     def size(self):
         """getter for size parameter"""
-        return self.height
+        return self.width
 
     @size.setter
     def size(self, value):
