c994344f604d3c03d55a46667f5dbe3abd5e34dc
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-02-28 01:53:46 +0300

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = Array.from(new Set(process.argv.slice(2))).sort((a, b) => a - b);
+const array = Array.from(new Set(process.argv.slice(2))).sort((a, b) => b - a);
 if (array.length == 0 || array.length == 1) {
   console.log(0);
 } else {
