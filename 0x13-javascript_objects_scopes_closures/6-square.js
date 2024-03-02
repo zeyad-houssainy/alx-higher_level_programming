66c4a196ca3eea1ae0ddc3fc5a3a322881564476
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-02 16:57:19 +0300

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
-class Square extends require('./4-rectangle.js') {
-  constructor (size) {
+module.exports = class Square extends require('./5-square.js') {
+  constructor (size){
     super(size, size);
   }
-
-
-
-  charPrint (c) {
-    if (c === undefined) {
+  charPrint (c){
+    if (c === undefined){
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
-      console.log(`${c}`.repeat(this.width));
+        console.log(`${c}`.repeat(this.width));
+      }
     }
   }
-  }
-
-module.exports = Square;
+}
