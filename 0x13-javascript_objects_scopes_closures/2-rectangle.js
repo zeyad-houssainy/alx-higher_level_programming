659ad359c52de13a2b1751e5203bc2532e99e4cb
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-02 14:57:04 +0300

    2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+    constructor(w, h){
+        if (w > 0 && h > 0){
+            this.width = w;
+            this.height = h;
+        }
+    }
+}
+
+const r2 = new Rectangle(2, -3);
+console.log(r2);
+console.log(r2.width);
+console.log(r2.height);
