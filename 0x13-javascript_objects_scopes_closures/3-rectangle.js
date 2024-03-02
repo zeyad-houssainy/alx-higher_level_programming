826a9b87947c9a8e122c40611b834b4e036a5c99
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-02 15:31:38 +0300

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,10 +7,19 @@ class Rectangle {
     }
   }
 
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
+  }
+
+  rotate(){
+    [this.height, this.width] = [this.width, this.height]
+  }
+
+  double(){
+    [this.height, this.width] = [this.height * 2, this.width * 2]
   }
 }
 module.exports = Rectangle;
