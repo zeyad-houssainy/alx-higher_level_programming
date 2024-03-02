aeae088ee26b10cd2f6a76ea0bd9985d70528158
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-02 23:31:20 +0300

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+// Fix: Convert CommonJS module to ES module
+exports.esrever = function (list) {
+    let newList = [];
+    for (let i of list) {
+        newList.unshift(i);
+    }
+    return newList;
+}
