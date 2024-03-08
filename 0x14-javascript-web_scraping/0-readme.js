f3c4ea2e176ac65f9d5e91be538ccdeb91a0a8f9
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-09 01:33:50 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error('Error ready the file');
+    console.log(err);
     return;
   }
   console.log(data);
