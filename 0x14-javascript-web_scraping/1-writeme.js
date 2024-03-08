f537f0217f9372dd2dd38a18a7dec7129bed4807
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-09 02:12:26 +0300

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
+const fs = require("fs");
+fs.writeFile(process.argv[2], process.argv[3], "utf8", function (err) {
   if (err) {
     console.log(err);
   }
