edb9d5e4fb1297ba9df6943ff62bcbc228d64d47
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-09 02:10:01 +0300

    1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filePath = process.argv[2];
-const fileContent = process.argv[3];
-fs.writeFile(filePath, fileContent, 'utf-8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
     console.log(err);
   }
