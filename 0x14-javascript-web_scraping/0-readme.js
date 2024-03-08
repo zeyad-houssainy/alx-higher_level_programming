6dbfda89d4a4054b1e453f01b1c39973797e9e32
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-09 01:32:40 +0300

    1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const fs = require("fs");
-fs.readFile(process.argv[2], "utf-8", (err, data) => {
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error("Error ready the file");
+    console.error('Error ready the file');
     return;
   }
   console.log(data);
--- 0x14-javascript-web_scraping/test.js
+++ 0x14-javascript-web_scraping/test.js
@@ -1,7 +1,7 @@
-const fs = require("fs");
-fs.readFile("test.txt", "utf-8", (err, data) => {
+const fs = require('fs');
+fs.readFile('test.txt', 'utf-8', (err, data) => {
   if (err) {
-    console.error("Error ready the file");
+    console.error('Error ready the file');
     return;
   }
   console.log(data);
