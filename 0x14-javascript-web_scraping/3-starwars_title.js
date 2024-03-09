0c40059c657fb5846f1965c9144fb9d745f7a4d4
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-09 18:46:38 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const starWarsUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+const starWarsUrl = `https://swapi-api.alx-tools.com/api/films/:${process.argv[2]}`;
 request(starWarsUrl, (_error, _response, body) => {
   if (error) {
     console.error(error);
