ebee2d683dc60965deb640f04fcc3d05d2670b70
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-09 18:49:49 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const request = require('request');
-const starWarsUrl = `https://swapi-api.alx-tools.com/api/films/:${process.argv[2]}`;
-request(starWarsUrl, (_error, _response, body) => {
+// uses Star Wars API to display title of film by id/release order
+const request = require("request");
+
+const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+request(url, function (error, response, body) {
   if (error) {
     console.error(error);
   }
-  const statWarsMovieData = JSON.parse(body);
-  const statWarsMovieName = statWarsMovieData.title;
-  console.log(statWarsMovieName);
+  const film = JSON.parse(body);
+  console.log(film.title);
 });
