9640336afa076957f94e28b5558880350ca86265
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-09 18:33:15 +0300

    1

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const request = require('request');
+const starWarsUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request(starWarsUrl, (_error, _response, body) => {
+  body = JSON.parse(body);
+  console.log(body.title);
+});
