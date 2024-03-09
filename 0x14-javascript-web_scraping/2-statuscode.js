813a0cfca73d5372013c346ad4d09aa6a947f173
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-09 16:14:19 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request(url, (_error, response) => {
+request.get(url, (_error, response) => {
   console.log(`code: ${response.statusCode}`);
 });
