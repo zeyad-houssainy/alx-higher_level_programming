3c1873630ac8ad4fc79a46028370dcb641982e43
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2024-03-09 17:08:03 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
+// displays response status code of a GET request
 const request = require('request');
-const url = process.argv[2];
-request.get(url, (error, response) => {
+
+request(process.argv[2], function (error, response, body) {
   if (error) {
-    console.log(error);
-  } else {
-    console.log(`code: ${response.statusCode}`);
+    console.error(error);
   }
+  console.log('code:', response && response.statusCode);
 });
